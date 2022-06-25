8a8e7306337256621aa58d875ef2efae29a4ae2b
wasifali591 <wasifali591@gmail.com>
2022-06-26 00:56:20 +0530

    Angular app for employee management

---
+++ src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Employee } from './employee';
+import { EmployeeService } from './employee.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+  public employees: Employee[] = [];
+
+  constructor(private employeeService: EmployeeService){}
+
+  ngOnInit(): void {
+      this.getEmployees();
+  }
+
+  public getEmployees(): void{
+    this.employeeService.getEmployees().subscribe(
+      (rsponse: Employee[]) => {
+        this.employees = rsponse;
+      },
+      (error: HttpErrorResponse) =>{
+        alert(error.message);
+      }
+    )
+  }
+}
