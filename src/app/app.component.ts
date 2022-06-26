5a1eed798bbff1b1eb95458d8ee2ebf83c60c3bf
wasifali591 <wasifali591@gmail.com>
2022-06-27 00:00:50 +0530

    card design to show the emoployee details

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Employee } from './employee';
 import { EmployeeService } from './employee.service';
 
@@ -10,6 +11,7 @@ import { EmployeeService } from './employee.service';
 })
 export class AppComponent implements OnInit{
   public employees: Employee[] = [];
+  public editEmployee: Employee ;
 
   constructor(private employeeService: EmployeeService){}
 
@@ -27,4 +29,61 @@ export class AppComponent implements OnInit{
       }
     )
   }
+
+  public onAddEmloyee(addForm: NgForm): void{
+
+    document.getElementById('add-employee-form')?.click();
+    this.employeeService.addEmployee(addForm.value).subscribe(
+      (response: Employee) => {
+        console.log(response);
+        this.getEmployees();
+        addForm.reset();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+        addForm.reset();
+      }
+    );
+  }
+
+  public onUpdateEmloyee(employee: Employee): void {
+    this.employeeService.updateEmployee(employee).subscribe(
+      (response: Employee) => {
+        console.log(response);
+        this.getEmployees();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+      }
+    );
+  }
+
+  public onDeleteEmloyee(){
+
+  }
+
+  public onOpenModal(employee: Employee, mode: string): void{
+
+    const button = document.createElement('button');
+    const container = document.getElementById('#main-container');
+
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle','model');
+
+    if(mode === 'add'){
+      button.setAttribute('data-target','#addEmployeeModal');
+    }
+    if(mode === 'edit'){
+      this.editEmployee = employee;
+      button.setAttribute('data-target','#updateEmployeeModal');
+    }
+    if(mode === 'delete'){
+      button.setAttribute('data-target','#deleteEmployeeModal');
+    }
+
+    container?.appendChild(button);
+  }
 }
+
+
